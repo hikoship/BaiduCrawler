1c255f0dc6048b0e1ae5070fb334819e2bbecfbc
hiko <hikoship@gmail.com>
2016-04-25 13:22:13 +0800

    first commit
    user can input keywords and number of results

---
+++ BaiduCrawler.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import urllib, urllib.request
+import sys
+from bs4 import BeautifulSoup
+
+
+word = input("Input your keywords: ") # keyword to be searched
+num = input("Input the number of results you want: ") # num of news to be crawled
+num = int(num)
+rn = 50 # num of news per page
+
+header = { 'Use-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6' }
+
+q = {}
+page = 1
+
+while num > 0:
+    q['word'] = word
+    if num > rn:
+        q['rn'] = rn
+    else:
+        q['rn'] = num
+    num = max(0, num - rn)
+    q['pn'] = str(page - 1) + '0' # match Baidu page parameter
+    q['tn'] = 'newstitle' # only shows title
+    q=urllib.parse.urlencode(q)
+    url = "http://news.baidu.com/ns?" + q
+
+    # parse the page
+    content = urllib.request.urlopen(url).read().decode('UTF-8')
+    soup = BeautifulSoup(content, 'html.parser')
+
+    for news in soup.select('.result'):
+
+        link = news.h3.a
+        tmpTitle = link.contents
+        title = ''
+        for e in tmpTitle:
+            title += str(e)
+        title = title.replace('<em>', '')
+        title = title.replace('</em>', '')
+        print (title)
+        print (link['href'])
